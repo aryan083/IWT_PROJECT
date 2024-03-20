ba6a885ae566982b948e4521e44816b8d30f0846
Umang Hirani <umanghirani.exe@gmail.com>
2024-03-20 23:17:52 +0530

    signup-page-created

---
+++ src/front-end/signup-page-files/input-decider.js
@@ -0,0 +1,35 @@
+//here will be the logic that decides which input field to be displayed
+
+// 1.1 Stduent Regesitation fields 
+// First Name + Last Name 
+// Enrollemnt No
+// GR. No.
+// Email
+// Passoword
+// Confirm Password
+
+// 2.1 Faculty Regesitation fields
+// First Name + Last Name
+// Faculty ID
+// Email
+// Passoword
+// Confirm Password
+
+// 3.1 Parent Regesitation fields
+// First Name + Last Name
+// Email
+// passoword
+
+//onselect of the dropdown of will select one of the three functions below
+
+function select_student(){
+
+}
+
+function select_parent(){
+
+}
+
+function select_faculty(){
+
+}
