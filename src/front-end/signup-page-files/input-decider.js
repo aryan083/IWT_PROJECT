7b6d3fed9cddfd23541d8db55002b0626a79154d
Umang Hirani <umanghirani.exe@gmail.com>
2024-03-21 23:23:07 +0530

    signup-page-dropdown-added
    dropdown is added !
    now all the fields are available for backend to be updated for all the three cases namely student parent and faculty
    using the js-file

--- src/front-end/signup-page-files/input-decider.js
+++ src/front-end/signup-page-files/input-decider.js
@@ -24,12 +24,101 @@
 
 function select_student(){
 
+    var html = `<div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+</div>
+
+<div class="input-group mb-2">
+    <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Enrollment No." name="enr_no" id="enr_no">
+</div>
+<div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+</div>
+<div class="input-group mb-3">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+</div>
+
+<div class="input-group mb-3">
+    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+</div>
+<div class="row">
+    <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
+</div>`
+
+
+
+document.getElementById("input-group-js-signup").innerHTML = html;
+document.getElementById("dropdown-signup-selector").innerText = "Student";
+
+
+    
+    
+
+
+
 }
 
 function select_parent(){
+    var html =           `    <div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+</div>
+
+<div class="input-group mb-2">
+    <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Guardian's Enrollment No." name="gaurd_id" id="gaurd_id">
+</div>
+<div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+</div>
+<div class="input-group mb-3">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+</div>
+
+<div class="input-group mb-3">
+    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+</div>
+<div class="row">
+    <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
+</div>
+
+</div>`
+
+
+
+document.getElementById("input-group-js-signup").innerHTML = html;
+document.getElementById("dropdown-signup-selector").innerText = "Parent";
 
 }
 
 function select_faculty(){
+    var html = `                        <div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+</div>
+
+<div class="input-group mb-2">
+    <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Faculty ID" name="fac_id" id="fac_id">
+</div>
+<div class="input-group mb-2">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+</div>
+<div class="input-group mb-3">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+</div>
+
+<div class="input-group mb-3">
+    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+</div>
+<div class="row">
+    <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
+</div>`
+
+
+
+document.getElementById("input-group-js-signup").innerHTML = html;
+document.getElementById("dropdown-signup-selector").innerText = "Faculty";
 
 }
+
+// <!-- input group that will be affected by the js file -->
+
+
+                        
