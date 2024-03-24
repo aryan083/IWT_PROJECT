de81b8d61d09b1f9fcc15654c9c59bddc371801f
Umang Hirani <umanghirani.exe@gmail.com>
2024-03-24 15:40:58 +0530

    add-project (add_collab add_mentor all other fields except recognition and external links)

---
+++ src/front-end/add-project-files/add-project-script.js
@@ -0,0 +1,26 @@
+function add_collab(){
+    // var student_name  = document.getElementById("collab-name").value;
+    // var role  = document.getElementById("collab-role").value;
+
+    // console.log(student_name,role);
+
+    var to_be_added_html_student = document.getElementById("default-add-collab-stuff").innerHTML;
+    var already_existing_html_student = document.getElementById("add-collab-field").innerHTML;
+    
+    new_html_for_collab=already_existing_html_student+to_be_added_html_student;
+    document.getElementById("add-collab-field").innerHTML = new_html_for_collab;
+    // console.log(new_html_for_collab);
+}
+function add_mentor(){
+    // var mentor_name  = document.getElementById("mentor-student_name").value;
+    // var role  = document.getElementById("collab-role").value;
+
+    // console.log(mentor_name_name);
+
+    var to_be_added_html_mentor = document.getElementById("default-add-mentor-stuff").innerHTML;
+    var already_existing_html_mentor = document.getElementById("add-mentor-field").innerHTML;
+    
+    new_html_for_mentor=already_existing_html_mentor+to_be_added_html_mentor;
+    document.getElementById("add-mentor-field").innerHTML = new_html_for_mentor;
+
+}
