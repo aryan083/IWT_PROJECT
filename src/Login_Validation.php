3c8be3a8fccf1e177e489b0ff8e48ccd77439498
aryan083 <aryanmahida2@gmail.com>
2024-03-24 14:31:58 +0530

    added sentized fileds in php also done some improvements in code

--- src/Login_Validation.php
+++ src/Login_Validation.php
@@ -2,88 +2,195 @@
 session_start();
 require_once "dbconnection.php";
 
-// Function to check student login
+
+//function to check if student password is incorrect
+function isStudentPasswordIncorrect($conn, $email, $password) {
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email' AND student_password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_password");//incorrect password
+    exit();
+}
+//function to check if faculty password is incorrect
+function isFacultyPasswordIncorrect($conn, $email, $password) {
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_email = '$email' AND faculty_password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_password");//incorrect password
+    exit();
+}
+
+//function to check if parent password is incorrect
+function isParentPasswordIncorrect($conn, $email, $password) {
+    $sql = "SELECT * FROM spms_parent WHERE parent_email = '$email' AND parent_password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_password");//incorrect password
+    exit();
+}
+
+
+//function to check if student email is incorrect
+function isStudentEmailIncorrect($conn, $email) {
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_email");//incorrect email
+    exit();
+}
+
+//function to check if faculty email is incorrect
+function isFacultyEmailIncorrect($conn, $email) {
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_email");//incorrect email
+    exit();
+}
+
+//function to check if parent email is incorrect
+function isParentEmailIncorrect($conn, $email) {
+    $sql = "SELECT * FROM spms_parent WHERE parent_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=incorrect_email");//incorrect email
+    exit();
+}
+
+
+//function to check if student doesn't exist
+function isStudentExist($conn, $email) {
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=user_doesnt_exist");//user doesn't exist
+    exit();
+}
+
+
+//function to check if faculty doesn't exist
+function isFacultyExist($conn, $email) {
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=user_doesnt_exist");//user doesn't exist
+    exit();
+}
+
+//function to check if parent doesn't exist
+function isParentExist($conn, $email) {
+    $sql = "SELECT * FROM spms_parent WHERE parent_email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        return;
+    }
+    header("Location: login-page.html?error=user_doesnt_exist");//user doesn't exist
+    exit();
+}
+
+//function for student login
 function studentLogin($conn, $email, $password) {
-    $sql = "SELECT * FROM spms_student WHERE student_email='$email'";
+    $sql = "SELECT * FROM spms_student WHERE student_email = '$email' AND student_password = '$password'";
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) == 1) {
+    if(mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $studentpassword_hash = $row['student_pasword'];
-        if (password_verify($password, $studentpassword_hash) && $email == $row['student_email']) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
+        $_SESSION['student_name']=$row['student_name'];
+        $_SESSION['enrollment_no']=$row['enrollment_no'];
+
+        header("Location: index.html");
+        exit();
     }
 }
 
-// Function to check faculty login
+//function for faculty login
 function facultyLogin($conn, $email, $password) {
-    $sql = "SELECT * FROM spms_faculty WHERE faculty_email ='$email'";
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_email = '$email' AND faculty_password = '$password'";
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) == 1) {
+    if(mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $facultypassword_hash = $row['faculty_password'];
-        if (password_verify($password, $facultypassword_hash) && $email == $row['faculty_email']) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
+        $_SESSION['faculty_name']=$row['faculty_name'];
+        $_SESSION['faculty_id']=$row['faculty_id'];
+
+        header("Location: index.html");
+        exit();
     }
 }
 
-// Function to check parent login
+//function for parent login
+
 function parentLogin($conn, $email, $password) {
-    $sql = "SELECT * FROM spms_parent WHERE parent_email ='$email'";
+    $sql = "SELECT * FROM spms_parent WHERE parent_email = '$email' AND parent_password = '$password'";
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) == 1) {
+    if(mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $parentpassword_hash = $row['parent_password'];
-        if (password_verify($password, $parentpassword_hash) && $email == $row['parent_email']) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
+        $_SESSION['parent_name']=$row['parent_name'];
+        $_SESSION['child_enrollment_no']=$row['child_enrollment_no'];
+
+        header("Location: index.html");
+        exit();
     }
 }
 
-// Main code execution
-if ($_REQUEST) {
-    $email = $_REQUEST['email'];
+//function to check if user is already logged in
+
+
+if($_REQUEST['login']){
+
+    $email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
     $password = $_REQUEST['password'];
 
-    if (strpos($email, '@marwadiuniversity.ac.in') !== false) {
-        if (studentLogin($conn, $email, $password)) {
-            echo "Login Successful";
-            header("Location: index.html");
-            exit();
-        } else {
-            echo "Invalid email or password";
-        }
-    } elseif (strpos($email, '@marwadieducation.edu.in') !== false) {
-        if (facultyLogin($conn, $email, $password)) {
-            echo "Login Successful";
-            header("Location: index.html");
-            exit();
-        } else {
-            echo "Invalid email or password";
-        }
-    } elseif (strpos($email, '@gmail.com') !== false) {
-        if (parentLogin($conn, $email, $password)) {
-            echo "Login Successful";
-            header("Location: index.html");
-            exit();
-        } else {
-            echo "Invalid email or password";
-        }
-    } else {
-        echo "Invalid email";
-    }
-}
 
+
+    if(strpos($email, '@marwadiuniversity.ac.in') !== false)
+    {
+        isStudentExist($conn, $email);
+        isStudentEmailIncorrect($conn, $email);
+        isStudentPasswordIncorrect($conn, $email, $password);
+        studentLogin($conn, $email, $password);
+    }
+    elseif(strpos($email, '@marwadieducation.edu.in') !== false)
+    {
+        isFacultyExist($conn, $email);
+        isFacultyEmailIncorrect($conn, $email);
+        isFacultyPasswordIncorrect($conn, $email, $password);
+        facultyLogin($conn, $email, $password);
+    }
+    else
+    {
+        isParentExist($conn, $email);
+        isParentEmailIncorrect($conn, $email);
+        isParentPasswordIncorrect($conn, $email, $password);
+        parentLogin($conn, $email, $password);
+    }
+
+    header("Location: login-page.html?error=login_failed");//login failed
+    exit();
+
+}
--- src/Sign_Up_Validation.php
+++ src/Sign_Up_Validation.php
@@ -3,31 +3,92 @@ require_once 'dbconnection.php';
 
 // Function to check if a student already exists in the database
 function isStudentExists($conn, $enrollment_no, $student_email, $student_personal_email, $student_phone_no) {
-    $sql = "SELECT * FROM spms_student WHERE student_enrollment_number = '$enrollment_no' OR student_email = '$student_email' OR student_personal_email = '$student_personal_email' OR student_phone_no = '$student_phone_no'";
+    $sql = "SELECT * FROM spms_student WHERE student_enrollment_number = '$enrollment_no'";
     $result = mysqli_query($conn, $sql);
-    return mysqli_num_rows($result) > 0;
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=student_enrollment_exists");//enollment number already exists in database
+        exit();
+    }
+    $sql = "SELECT * FROM spms_student WHERE OR student_email = '$student_email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=student_email_exists");// collage email already exists in database 
+        exit();
+    }
+
+    $sql = "SELECT * FROM spms_student WHERE student_personal_email = '$student_personal_email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=student_personal_email_exists");// personal email already exists in database
+        exit();
+    }
+    
+    $sql = "SELECT * FROM spms_student WHERE student_phone_no = '$student_phone_no'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=student_phone_no_exists");// phone number already exists in database
+        exit();
+    }
+
+    return ;
 }
 
 // Function to check if a faculty already exists in the database
 function isFacultyExists($conn, $faculty_id, $faculty_email) {
-    $sql = "SELECT * FROM spms_faculty WHERE faculty_id = '$faculty_id' OR faculty_email = '$faculty_email'";
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_id = '$faculty_id'";
     $result = mysqli_query($conn, $sql);
-    return mysqli_num_rows($result) > 0;
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=faculty_id_exists");//faculty id already exists in database
+        exit();
+    }
+    $sql = "SELECT * FROM spms_faculty WHERE faculty_email = '$faculty_email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=faculty_email_exists");//faculty email already exists in database
+        exit();
+    }
+
+    return;
 }
 
 // Function to check if a parent already exists in the database
 function isParentExists($conn, $child_enrollment_no, $parent_email, $parent_phone_no) {
-    $sql = "SELECT * FROM spms_parent WHERE child_enrollement_number = '$child_enrollment_no' OR parent_email = '$parent_email' OR parent_phone_number = '$parent_phone_no'";
+    $sql = "SELECT * FROM spms_parent WHERE child_enrollement_number = '$child_enrollment_no'";
     $result = mysqli_query($conn, $sql);
-    return mysqli_num_rows($result) > 0;
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=child_enrollment_exists");//child enrollment number already exists in database
+        exit();
+    }
+    $sql = "SELECT * FROM spms_parent WHERE parent_email = '$parent_email'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=parent_email_exists");//parent email already exists in database
+        exit();
+    }
+    $sql = "SELECT * FROM spms_parent WHERE parent_phone_number = '$parent_phone_no'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) 
+    {
+        header("Location: signup-page.html?error=parent_phone_no_exists");//parent phone number already exists in database
+        exit();
+    }
+
+    return;
 }
 
 // Function to insert a new student record
 function addStudent($conn, $student_name, $enrollment_no, $student_email, $student_personal_email, $student_phone_no, $student_password) {
-    if (isStudentExists($conn, $enrollment_no, $student_email, $student_personal_email, $student_phone_no)) {
-        return false; // Entry already exists
-    }
-
+    
+    isStudentExists($conn, $enrollment_no, $student_email, $student_personal_email, $student_phone_no);
+  
     // Insert new record
     $hashedpassword = password_hash($student_password, PASSWORD_DEFAULT);
     $student_signup = date("Y-m-d H:i:s");
@@ -48,9 +109,8 @@ function addStudent($conn, $student_name, $enrollment_no, $student_email, $stude
 
 // Function to insert a new faculty record
 function addFaculty($conn, $faculty_name, $faculty_id, $faculty_email, $faculty_password) {
-    if (isFacultyExists($conn, $faculty_id, $faculty_email)) {
-        return false; // Entry already exists
-    }
+    
+    isFacultyExists($conn, $faculty_id, $faculty_email);
 
     // Insert new record
     $hashedpassword = password_hash($faculty_password, PASSWORD_DEFAULT);
@@ -70,9 +130,7 @@ function addFaculty($conn, $faculty_name, $faculty_id, $faculty_email, $faculty_
 
 // Function to insert a new parent record
 function addParent($conn, $parent_name, $child_enrollment_no, $parent_email, $parent_phone_no, $parent_password) {
-    if (isParentExists($conn, $child_enrollment_no, $parent_email, $parent_phone_no)) {
-        return false; // Entry already exists
-    }
+    isParentExists($conn, $child_enrollment_no, $parent_email, $parent_phone_no);
 
     // Insert new record
     $hashedpassword = password_hash($parent_password, PASSWORD_DEFAULT);
@@ -98,15 +156,37 @@ if ($_REQUEST)
 
         if (strpos($email, '@marwadiuniversity.ac.in') !== false)
         {
-            $result = addStudent($conn, $_REQUEST['student_name'], $_REQUEST['enr_no'], $_REQUEST['email'], $_REQUEST['personal_email'], $_REQUEST['phone_no'], $_REQUEST['password']);
-        } 
-        elseif (strpos($email, '@marwadieducation.edu.in') !== false) 
-        {
-            $result = addFaculty($conn, $_REQUEST['full_name'], $_REQUEST['fac_id'], $_REQUEST['email'], $_REQUEST['password']);
+
+            $student_name = filter_var($_REQUEST['full_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $student_enrollment_no = filter_var($_REQUEST['enr_no'], FILTER_SANITIZE_NUMBER_INT);
+            $student_email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+            $student_personal_email = filter_var($_REQUEST['personal_email'], FILTER_SANITIZE_EMAIL);
+            $student_phone_no = filter_var($_REQUEST['phone_no'], FILTER_SANITIZE_NUMBER_INT);
+            $student_password = $_REQUEST['password'];
+
+            $result = addStudent($conn, $student_name, $enrollment_no, $student_email, $student_personal_email, $student_phone_no, $student_password);
         }
-         else 
+
+        elseif (strpos($email, '@marwadieducation.edu.in') !== false)
         {
-            $result = addParent($conn, $_REQUEST['full_name'], $_REQUEST['child_id'], $_REQUEST['email'], $_REQUEST['phone_no'], $_REQUEST['password']);
+            $parent_name = filter_var($_REQUEST['full_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $child_enrollment_no = filter_var($_REQUEST['child_id'], FILTER_SANITIZE_NUMBER_INT);
+            $parent_email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+            $parent_phone_no = filter_var($_REQUEST['phone_no'], FILTER_SANITIZE_NUMBER_INT);
+            $parent_password = $_REQUEST['password'];
+
+            $result = addFaculty($conn, $faculty_name, $faculty_id, $faculty_email, $faculty_password);
+        } 
+
+        else
+        {
+
+            $faculty_name = filter_input($_REQUEST['full_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $faculty_id = filter_var($_REQUEST['fac_id'], FILTER_SANITIZE_NUMBER_INT);
+            $faculty_email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+            $faculty_password = $_REQUEST['password'];
+
+            $result = addParent($conn, $parent_name, $child_enrollment_no, $parent_email, $parent_phone_no, $parent_password);
         }
 
             if ($result) 
