a5e0c9ed65a8970f430ca2328578aba638820faf
aryan083 <aryanmahida2@gmail.com>
2024-04-06 22:33:11 +0530

    sing up and login working with cookies and sessions

--- src/Login_Validation.php
+++ src/Login_Validation.php
@@ -2,6 +2,23 @@
 session_start();
 require_once "dbconnection.php";
 
+if(isset($_POST['logout'])) {
+    // Unset all of the session variables
+    $_SESSION = array();
+
+    // Destroy the session
+    session_destroy();
+
+    // Clear any existing cookies
+    setcookie('studentEmail', '', time() - 3600, '/');
+    setcookie('facultyEmail', '', time() - 3600, '/');
+    setcookie('parentEmail', '', time() - 3600, '/');
+
+    // Redirect to the login page
+    header("Location: login-page.html");
+    exit();
+}
+
 // Function to validate input data
 function validate_input($data) {
     $data = trim($data);
