cd2f00cf8dd1379cb62fd2f67906095f78620de3
aryan083 <aryanmahida2@gmail.com>
2024-04-17 14:01:42 +0530

    login and sign up working with a small bug

--- src/front-end/signup-page-files/input-decider.js
+++ src/front-end/signup-page-files/input-decider.js
@@ -26,21 +26,23 @@
 function select_student(){
 
     var html = `<div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="student_full_name" id="full_name">
 </div>
 
+    <input type="hidden" name="role" value="student"></input>
+
 <div class="input-group mb-2">
-    <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Enrollment No." name="enr_no" id="enr_no">
+    <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Enrollment No." name="student_enr_no" id="enr_no">
 </div>
 <div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="student_email" id="email">
 </div>
 <div class="input-group mb-3">
-    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="student_password" id="password">
 </div>
 
 <div class="input-group mb-3">
-    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+    <input type="submit" class="btn btn-lg btn-primary w-100 fs-6"></input>
 </div>
 <div class="row">
     <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
@@ -55,21 +57,23 @@ document.getElementById("dropdown-signup-selector").innerText = "Student";
 
 function select_parent(){
     var html =           `    <div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="parent_full_name" id="full_name">
 </div>
+
+<input type="hidden" name="role" value="parent"></input>
 
 <div class="input-group mb-2">
     <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Child's Enrollment No." name="child_id" id="child_id">
 </div>
 <div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="parent_email" id="email">
 </div>
 <div class="input-group mb-3">
-    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="parent_password" id="password">
 </div>
 
 <div class="input-group mb-3">
-    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+    <input type="submit" class="btn btn-lg btn-primary w-100 fs-6"></input>
 </div>
 <div class="row">
     <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
@@ -86,21 +90,23 @@ document.getElementById("dropdown-signup-selector").innerText = "Parent";
 
 function select_faculty(){
     var html = `                        <div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="full_name" id="full_name">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Full Name" name="faculty_full_name" id="full_name">
 </div>
+
+<input type="hidden" name="role" value="faculty"></input>
 
 <div class="input-group mb-2">
     <input type="number" class="form-control form-control-lg bg-light fs-6" placeholder="Faculty ID" name="fac_id" id="fac_id">
 </div>
 <div class="input-group mb-2">
-    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="email" id="email">
+    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email Address" name="faculty_email" id="email">
 </div>
 <div class="input-group mb-3">
-    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" id="password">
+    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="faculty_password" id="password">
 </div>
 
 <div class="input-group mb-3">
-    <button class="btn btn-lg btn-primary w-100 fs-6" value="Submit">Sign Up</button>
+    <input type="submit" class="btn btn-lg btn-primary w-100 fs-6"></input>
 </div>
 <div class="row">
     <small class="d-flex justify-content-center">Already a user ? <a href="login-page.html"> Login</a></small>
