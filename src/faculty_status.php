d3ddaede678bedeb6ee36414ea04d9620e5ad59e
aryan083 <aryanmahida2@gmail.com>
2024-05-09 04:13:58 +0530

    nav bar added in all the files

--- src/faculty_status.php
+++ src/faculty_status.php
@@ -8,8 +8,51 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="front-end/add-project-files/add-project-styles.css">
+    <link rel="stylesheet" href="front-end/navbar-styles.css">
 </head>
 <body>
+<nav class="navbar shadow navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand me-auto fs-3" id="spmsbranding" href="index.html">SPMS</a>
+
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="spmsbranding">SPMS</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2 active" aria-current="page" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="project_feed.html">Feed</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="project_library.php">Library</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="add_project.html">Add Project</a>
+            </li>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2" href="about.html">About</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <?php if (isset($_SESSION['email'])): ?>
+            <a href="profile.php" class="login-button">Profile</a>
+        <?php else: ?>
+            <a href="login-page.html" class="login-button">Login</a>
+        <?php endif; ?>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+  
 
     <div class="container mt-5">
         <h2>Faculty Status</h2>
