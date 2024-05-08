87206e091be4f831d2eafeb8d12cf363f75139bc
aryan083 <aryanmahida2@gmail.com>
2024-05-09 03:57:07 +0530

    navbar issue solvedw

--- src/index2.php
+++ src/index2.php
@@ -59,8 +59,7 @@ require_once 'dbconnection.php';
         <?php else: ?>
             <a href="login-page.html" class="login-button">Login</a>
         <?php endif; ?>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="offcanvasNavbar"
-        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     </div>
